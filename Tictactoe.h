100af621a3ea451dbe7d6bfca3ad6615ee05a9d7
Gameforlife <nourhanfahmy8@gmail.com>
2024-12-02 16:22:52 +0200

    Tictactoe

--- Tictactoe.h
+++ Tictactoe.h
@@ -1,8 +1,8 @@
-//
-// Created by nourh on 02/12/2024.
-//
-
 #ifndef BOARD_GAMES_TICTACTOE_H
 #define BOARD_GAMES_TICTACTOE_H
 
+template <class T>
+class Tictactoe {
+
+};
 #endif //BOARD_GAMES_TICTACTOE_H
