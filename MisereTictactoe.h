cf6c9d7950cbc284e2461b0b914dea8396fe2f6d
Gameforlife <nourhanfahmy8@gmail.com>
2024-12-07 14:04:29 +0200

    Misere Tic Tac Toe

--- MisereTictactoe.h
+++ MisereTictactoe.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <iomanip>
 
+//Player<char>* global_Players[2];
+
 // class of Tic Tac Toe
 template <typename T>
 class MisereTicTacToeBoard: public Board<T> {
@@ -41,6 +43,7 @@ public:
 
 /*----------------------- IMPLEMENTATION -----------------------*/
 template <typename T>
+// initialize the board with empty spaces
 MisereTicTacToeBoard <T>::MisereTicTacToeBoard(){
     this -> rows = 3;
     this-> columns = 3;
@@ -51,6 +54,7 @@ MisereTicTacToeBoard <T>::MisereTicTacToeBoard(){
             this->board[i][j] = ' ';
         }
     }
+    // initialize the number of moves to 0
     this->n_moves = 0;
 }
 
@@ -61,11 +65,13 @@ bool MisereTicTacToeBoard<T> :: update_board(int x, int y, T symbol){
     // if the user inputs a row index less than 0 or greater than 2 --> out of bounds
     // or if the user inputs a column index less than 0 or greater than 2 --> out of bounds
     // or if the user chooses a cell which isn't empty --> move can't be made
-    if (x < 0 || x >= 3 || y < 0 || y >= 3 || this->board[x][y] != ' ') {
-        cout << "\n--> Invalid input!! Please input another cell\n";
-        return false; // Invalid move
+    if (x < 0 || x >= 3 || y < 0 || y >= 3) {
+        return false;
     }
-
+    // The user chooses a cell which isn't empty meaning it has been chosen before --> move can't be made
+    else if(this->board[x][y] != ' '){
+        return false;
+    }
     // the cell will be updated with the symbol --> either X or O according to each player
     this->board[x][y] = toupper(symbol);
     // the number of moves will be incremented until it reaches 9 when the game ends
@@ -77,13 +83,14 @@ bool MisereTicTacToeBoard<T> :: update_board(int x, int y, T symbol){
 template <typename T>
 void MisereTicTacToeBoard<T> :: display_board() {
 
+    // outputting a message for the user to tell him which move they are at
+    cout << "\n --- The board at move " << this->n_moves << " ---\n";
     // output the column index numbers at the top for better visibility
     cout << "    ";
     for(int x=0;x<3;x++){
         cout  << x << "   ";
     }
     cout << endl;
-
     // output the grid
     for (int i = 0; i < 3; i++) {
         // output the row index number for better visibility for the user
@@ -101,7 +108,6 @@ void MisereTicTacToeBoard<T> :: display_board() {
         if (i < 2) cout << "  -------------\n";
     }
     cout << endl;
-
 }
 
 // return true if 9 moves are done and no winner
@@ -114,14 +120,9 @@ bool MisereTicTacToeBoard<T>::is_draw() {
 // check if the game is over
 template <typename T>
 bool MisereTicTacToeBoard<T>::game_is_over() {
-    //return this-> n_moves == 9;
+    // either there is a winning player or the game is a draw
     return is_win() || is_draw();
 }
-
-
-
-
-
 
 
 // counting three-in-a-rows for each player
@@ -181,28 +182,23 @@ int MisereTicTacToeBoard<T>::counter_three_in_a_row(T symbol) {
 }
 
 
-
-
 template <typename T>
 bool MisereTicTacToeBoard<T>:: is_win(){
-    //int firstplayer = ;
-    //int secondplayer = counter_three_in_a_row('O');
-
-    // player 1 loses, player 2 wins
-        if(counter_three_in_a_row('X') > 0 && counter_three_in_a_row('O') == 0){
-            return true;
-        }
-
-        // player 2 loses, player 1 wins
-        if(counter_three_in_a_row('O') > 0 ){
-            return true;
-        }
+    int first = counter_three_in_a_row('X');
+    int second = counter_three_in_a_row('O');
+    // first player has a three in a row and second player doesn't
+    // So first player lost
+    if(first > 0 && second== 0){
+        *global_Players[1] = *global_Players[1];
+        return true;
+    }
+    // second player lost and first player is the winner
+    else if(second>0){
+        *global_Players[1] = *global_Players[0];
+        return true;
+    }
     return false;
 }
-
-
-
-
 
 // constructor for tic Tac Toe player
 template <typename T>
@@ -212,11 +208,29 @@ MisereTicTacToePlayer<T>::MisereTicTacToePlayer(string name, T symbol) : Player<
 // Get move from a human player
 template <typename T>
 void MisereTicTacToePlayer<T>::getmove(int& x, int& y) {
-    cout << "Please enter the cell index you want separated with a space\n";
+    cout << "Player " << this->getname() << ": Please enter the cell index you want separated with a space\n";
     cin >> x;
     cin >> y;
 }
 
+template <class T>
+class Random_PlayerMisere : public RandomPlayer<T> {
+public:
+    Random_PlayerMisere(T symbol) : RandomPlayer<T>(symbol) {
+        this->dimension = 3;
+        this->name = "Random Computer Player";
+        srand(static_cast<unsigned int>(time(0)));  // Seed the random generator
+    }
+    void getmove(int& x, int& y) override {
+        // Random row (0 to 2)
+        x = rand() % this->dimension;
+        // Random column (0 to 2)
+        y = rand() % 3;
+    }
+
+};
+
+/*
 template <typename T>
 MisereTicTacToeRandomPlayer<T>::MisereTicTacToeRandomPlayer(T symbol) : RandomPlayer<T>(symbol) {
     this->dimension = 5;
@@ -232,5 +246,6 @@ void MisereTicTacToeRandomPlayer<T>::getmove(int& x, int& y) {
         y = rand() % 5;
     }while (this->boardPtr->update_board(x, y, this->getsymbol()) == false);
 }
+*/
 
 #endif //BOARD_GAMES_MISERETICTACTOE_H
