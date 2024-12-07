66caf2832fcd659cb337235d52876bc0fd465ae8
Gameforlife <nourhanfahmy8@gmail.com>
2024-12-07 22:09:07 +0200

    4x4 Tic Tac Toe

--- Problem_7.h
+++ Problem_7.h
@@ -15,7 +15,6 @@ public:
     bool is_win();
     bool is_draw();
     bool game_is_over();
-    void get_tokenposition(int& x, int& y);
     int counter_three_in_a_row(T symbol);
 };
 
@@ -26,7 +25,6 @@ class Four_times_Four_Player : public Player <T>{
 public:
     Four_times_Four_Player(string name, T symbol);
     void getmove(int &x, int&y);
-    void get_tokenposition(int& x, int& y);
 };
 
 
@@ -36,13 +34,13 @@ class Random : public Random_Player <T>{
 public:
     Random(T symbol);
     void getmove(int& x,int& y);
-    void get_tokenmove(int& x, int& y);
 };
 
 
 
 
 /*----------------------- IMPLEMENTATION -----------------------*/
+
 template <typename T>
 // initialize the board with empty spaces
 Four_times_Four_Board <T>::Four_times_Four_Board(){
@@ -52,70 +50,174 @@ Four_times_Four_Board <T>::Four_times_Four_Board(){
     for (int i = 0; i < this->rows; i++) {
         this->board[i] = new T[this->columns];
         for (int j = 0; j < this->columns; j++) {
+            // initialize the tokens of 'x', player 1 in their initial position
             if((i==0 && j == 0) || (i==0 && j == 2) || (i==3 && j == 1) || (i==3 && j == 3)){
                 this->board[i][j] = 'O';
             }
+            // initialize the tokens of 'O', player 2 in their initial position
             else if((i==0 && j == 1) || (i==0 && j == 3) || (i==3 && j == 0) || (i==3 && j == 2)){
                 this->board[i][j] = 'X';
 
             }else{
+                // initialize the rest of the cells with empty space
                 this->board[i][j] = ' ';
             }
         }
     }
-    // initialize the number of moves to be equal 0
+    // initialize the number of moves to be equal 0, to output it for the player
     this->n_moves = 0;
 }
 
+
 template <typename T>
-// updating the board with each input move by the computer player or the user
-bool Four_times_Four_Board<T> :: update_board(int x, int y, T symbol){
+bool Four_times_Four_Board<T>::update_board(int x, int y, T symbol) {
+    int CurrentX, CurrentY;
 
-    // player 1's turn
-    if(symbol =='X') {
+    cout << "\n-> Enter the token's current position (row and column) that you want to move: \n";
+    // inputting the index position of the current values of X or O
+    cin >> CurrentX >> CurrentY;
 
-        // if the user inputs a row index less than 0 or greater than 4 --> out of bounds
-        // or if the user inputs a column index less than 0 or greater than 4 --> out of bounds
-        if (x < 0 || x >= 5 || y < 0 || y >= 5) {
-            return false;
-        }
-        // The user chooses a cell which isn't empty meaning it has been chosen before --> move can't be made
-        if (this->board[x][y] != ' ') {
-            return false;
-        }
-
+    // validation check to make sure the current token input and the new position input are both within the bounds
+    if (CurrentX < 0 ||  CurrentX>= this->rows ||  CurrentY < 0 ||  CurrentY >= this->columns ||
+        x < 0 || x >= this->rows || y < 0 || y >= this->columns) {
+        return false;
     }
 
+    // check if the current input token is equal to the symbol of the player, if not then the input is invalid
+    if (this->board[CurrentX][ CurrentY] != symbol) {
+        cout << "\n^^ Invalid input token, you must move your own token. ^^\n";
+        return false;
+    }
 
-    // the cell will be updated with the symbol if input is valid --> either X or O according to each player
+    // if the input cell is not empty, this means the cell is already occupied
+    if (this->board[x][y] != ' ') {
+        cout << "\n^^ Invalid move, the cell is not empty ^^\n";
+        return false;
+    }
+
+    // getting absolute difference of the input token cell and new cell position
+    // if the difference is equal to 1, then the 2 input points are either vertically above each other or horizontally next to each other
+    // if the difference isn't equal to 1 then the input is not valid
+    if (!((abs(CurrentX - x) == 1 &&  CurrentY == y) || (abs( CurrentY - y) == 1 && CurrentX == x))) {
+        cout << "\n^^ Invalid move, you can only move to adjacent cells horizontally or vertically ^^\n";
+        return false;
+    }
+
+    // making the current token empty
+    this->board[CurrentX][CurrentY] = ' ';
+    // making the new cell with the symbol of the player
     this->board[x][y] = toupper(symbol);
-    // the number of moves will be incremented until it reaches 24 when the game ends
     this->n_moves++;
     return true;
 }
 
 
 
+template <typename T>
+// displaying the board
+void Four_times_Four_Board<T> :: display_board() {
 
+    // outputting a message for the user to tell him which move they are at
+    cout << "\n --- The board at move " << this->n_moves << " ---\n";
+    // output the column index numbers at the top for better visibility for the user
+    cout << "    ";
+    for(int x=0;x<4;x++){
+        cout  << x << "   ";
+    }
+    cout << endl;
+
+    // output the grid
+    for (int i = 0; i < 4; i++) {
+        // output the row index number for better visibility for the user
+        cout << i << " | ";
+        for (int j = 0; j < 4; j++) {
+
+            //outputting the contents of each cell
+            cout << this->board[i][j];
+            //putting a separator between each column cell
+            if (j < 3) cout << " | ";
+        }
+        cout << " |";
+        cout << "\n";
+        //putting a separator between each row
+        if (i < 3) cout << "  -----------------\n";
+    }
+    cout << endl;
+
+}
+
+
+// counting three-in-a-rows for each player
+// will count for each player the three-in-a-rows in columns vertically and horizontally
+template <typename T>
+int Four_times_Four_Board<T>::counter_three_in_a_row(T symbol) {
+    int counter = 0;
+
+    // checking horizontally for three in a row in rows
+    for (int i = 0; i < this->rows; i++) {
+        // counting for 3 cells only each time, so will only check for 3 columns everytime
+        for (int j = 0; j<= this->columns-3; j++) {
+
+            // if the 3 cells next to each other are the same symbol, the counter will be incremented
+            if ( (this->board[i][j] == symbol) && (this->board[i][j + 1] == symbol) && (this->board[i][j + 2] == symbol)) {
+                counter++;
+            }
+        }
+    }
+
+    // Check vertically for three in a row in columns
+    for (int j = 0; j < this->columns; j++) {
+        // will only check for 3 cells each time, so will only check for 3 row cells each time
+        for (int i = 0; i <= this->rows-3; i++) {
+
+            // if the 3 cells below each other are the same symbol, the counter will be incremented
+            if ( (this->board[i][j] == symbol) && (this->board[i + 1][j] == symbol) && (this->board[i + 2][j] == symbol)) {
+                counter++;
+            }
+        }
+    }
+
+    return counter;
+}
+
+
+template <typename T>
+bool Four_times_Four_Board<T>:: is_win(){
+    int first = counter_three_in_a_row('X');
+    int second = counter_three_in_a_row('O');
+    // first player has a three in a row and second player doesn't
+    // So first player won
+    if(first > 0){
+        *global_Players[1] = *global_Players[0];
+        return true;
+    }
+    // second player won
+    else if(second>0){
+        *global_Players[1] = *global_Players[1];
+        return true;
+    }
+    return false;
+}
+
+
+// there is now draw in the game, so the is draw is always false
+template <typename T>
+bool Four_times_Four_Board<T>::is_draw() {
+    return  false;
+}
+
+
+// check if the game is over meaning that a player has won
+template <typename T>
+bool Four_times_Four_Board<T>::game_is_over() {
+    return is_win() || is_draw();
+}
 
 
 
 // constructor for tic Tac Toe player
 template <typename T>
 Four_times_Four_Player<T>::Four_times_Four_Player(string name, T symbol) : Player<T>(name, symbol) {}
-
-// Get move from a human player
-// getting the token the user wants to move
-template <typename T>
-void Four_times_Four_Player<T>::get_tokenposition(int& x, int& y) {
-    // player 1's turn
-    if(this-> getsymbol() == "X"){
-        cout << "\nPlayer " << this-> getname() << ": Please enter the cell index of the token you want to move separated with a space\n";
-        cin >> x;
-        cin >> y;
-    }
-
-}
 
 
 
