461b8438576dd366dc8b610023b9ecce7e0faf56
Nourhanfahmy8 <161892807+Nourhanfahmy8@users.noreply.github.com>
2024-12-12 15:02:16 +0200

    Problem 7

--- Problem_7.h
+++ Problem_7.h
@@ -4,13 +4,14 @@
 #include <iostream>
 #include <iomanip>
 
+/// Problem 7: 4*4 Tic Tac Toe ///
 
-// boolean flag for each player to check if the player is a random player or human player
 
+Player<char>* global_Players[2];
 static bool israndom_player1 = false;
 static bool israndom_player2 = false;
 
-// class of 4x4 Tic Tac Toe
+/// class of 4x4 Tic Tac Toe
 template <typename T>
 class Four_times_Four_Board: public Board<T>{
 public:
@@ -24,7 +25,7 @@ public:
 };
 
 
-// Tic Tac Toe player class
+/// Tic Tac Toe player class
 template <typename T>
 class Four_times_Four_Player : public Player <T>{
 public:
@@ -33,7 +34,7 @@ public:
 };
 
 
-// Tic Tac Toe random player
+/// Tic Tac Toe random player class
 template <typename T>
 class Random_fourtimesfour : public RandomPlayer <T>{
 public:
@@ -42,8 +43,28 @@ public:
 };
 
 
-/*----------------------- IMPLEMENTATION -----------------------*/
 
+/*----------- IMPLEMENTATION -----------*/
+
+
+/// random player initialization
+template <class T>
+Random_fourtimesfour<T> :: Random_fourtimesfour(T symbol) : RandomPlayer<T>(symbol) {
+    this->dimension = 4;
+    this->name = "Random Computer Player";
+    srand(static_cast<unsigned int>(time(0)));  // Seed the random generator
+}
+
+/// getting a random move from the random computer player
+template <typename T>
+void Random_fourtimesfour<T>::getmove(int& x, int& y) {
+    // Random row index
+    x = rand() % this->dimension;
+    // Random column index
+    y = rand() % this->dimension;
+}
+
+/// initializing the board of the game with empty spaces
 template <typename T>
 // initialize the board with empty spaces
 Four_times_Four_Board <T>::Four_times_Four_Board(){
@@ -68,11 +89,11 @@ Four_times_Four_Board <T>::Four_times_Four_Board(){
             }
         }
     }
-    // initialize the number of moves to be equal 0, to output it for the player
+    // initialize the number of moves to be equal 0, it is output to the user to know how many turns have they played
     this->n_moves = 0;
 }
 
-
+/// updating the board after each players turn after validation
 template <typename T>
 bool Four_times_Four_Board<T>::update_board(int x, int y, T symbol) {
 
@@ -80,20 +101,62 @@ bool Four_times_Four_Board<T>::update_board(int x, int y, T symbol) {
     int CurrentX, CurrentY;
 
     // Determine whose turn it is --> if player 1 (even moves) or player 2 (odd moves)
-
     // even moves so player 1's turn
     if (this->n_moves % 2 == 0) {
         // player 1 is a human
         if (!israndom_player1) {
-            cout << "\n-> Player 1, please enter the cell index of the token you want to move (x y):\n";
-            // getting the current cell index
-            cin >> CurrentX >> CurrentY;
+
+            //cout << "\n-> Player '1' : Please enter the cell index of the token you want to move (x y):\n";
+            while (true) {
+                // getting the current cell index
+                cin >> CurrentX >> CurrentY;
+                // validation check on the input
+
+                if (cin.fail() || CurrentX < 0 || CurrentX >= 4 || CurrentY < 0 || CurrentY >= 4) {
+                    cerr << "\nInvalid input! Please input a valid cell index between 0 and 3 inclusive\n" << endl;
+                    cin.clear();          // Clear the input stream
+                    cin.ignore(1000, '\n'); // Ignore remaining input
+                    continue;
+                } else if (this->board[CurrentX][CurrentY] != symbol) {
+                    // The player chooses a cell that doesn't contain his symbol --> so current cell cant be chosen
+                    cerr << "\nInvalid chosen cell, you have to choose a cell containing your token\n";
+                    continue;
+
+                } else break;
+            }
+
             // getting the index the player wants to move the tokens to
             cout << "\n-> Now please enter the target position you want to move the token to (x y):\n";
-            cin >> x >> y;
+            cout << "** Note: The move must be adjacent (horizontal or vertical)\n";
+
+            while (true) {
+                cin >> x >> y;
+                // validation check on the input
+
+                if (cin.fail() || x < 0 || x >= 4 || y < 0 || y >= 4) {
+                    cerr << "\nInvalid input! Please input a valid cell index between 0 and 3 inclusive\n" << endl;
+                    cin.clear();          // Clear the input stream
+                    cin.ignore(1000, '\n'); // Ignore remaining input
+                    continue;
+                } else if (this->board[x][y] != ' ') {
+                    // The user chooses a cell which isn't empty meaning --> move can't be made
+                    cerr << "\nThis cell has already been chosen, Please choose another cell\n";
+                    continue;
+                }
+
+                // validation check to make sure the move is adjacent (horizontal or vertical)
+                if ((abs(CurrentX - x) == 1 && CurrentY == y) || (abs(CurrentY - y) == 1 && CurrentX == x)) {
+                    // if yes, the move will be stored
+                    break;
+                } else {
+                    cerr << "\nInvalid move, please choose another cell as you can only move to adjacent cells horizontally or vertically\n";
+                    continue;
+                }
+            }
 
             // else Player 1 is random
         } else {
+
             // getting a random current index for player one
             do {
                 CurrentX = rand() % 4;
@@ -101,7 +164,7 @@ bool Four_times_Four_Board<T>::update_board(int x, int y, T symbol) {
                 // validation check to make sure the index chosen randomly is the same symbol for Player 1 -> 'X'
             } while (this->board[CurrentX][CurrentY] != symbol);
 
-            // getting the random target position
+            // getting the random target position index
             do {
                 x = rand() % 4;
                 y = rand() % 4;
@@ -113,20 +176,61 @@ bool Four_times_Four_Board<T>::update_board(int x, int y, T symbol) {
     else {
         // player 2 is a human
         if (!israndom_player2) {
-            cout << "\n--> Player 2, please enter the cell index of the token you want to move (x y):\n";
-            cin >> CurrentX >> CurrentY;
+
+            //cout << "\n-> Player '2' : Please enter the cell index of the token you want to move (x y):\n";
+            while (true) {
+                // getting the current cell index
+                cin >> CurrentX >> CurrentY;
+                // validation check on the input
+
+                if (cin.fail() || CurrentX < 0 || CurrentX >= 4 || CurrentY < 0 || CurrentY >= 4) {
+                    cerr << "\nInvalid input! Please input a valid cell index between 0 and 3 inclusive\n" << endl;
+                    cin.clear();          // Clear the input stream
+                    cin.ignore(1000, '\n'); // Ignore remaining input
+                    continue;
+                } else if (this->board[CurrentX][CurrentY] != symbol) {
+                    // The player chooses a cell that doesn't contain his symbol --> so current cell cant be chosen
+                    cerr << "\nInvalid chosen cell, you have to choose a cell containing your token\n";
+                } else break;
+            }
+
+            // getting the index the player wants to move the tokens to
             cout << "\n-> Now please enter the target position you want to move the token to (x y):\n";
-            cin >> x >> y;
+            cout << "** Note: The move must be adjacent (horizontal or vertical)\n";
+            while (true) {
+                cin >> x >> y;
+                // validation check on the input
+                if (cin.fail() || x < 0 || x >= 4 || y < 0 || y >= 4) {
+                    cerr << "\nInvalid input! Please input a valid cell index between 0 and 4 inclusive\n" << endl;
+                    cin.clear();          // Clear the input stream
+                    cin.ignore(1000, '\n'); // Ignore remaining input
+                    continue;
+                } else if (this->board[x][y] != ' ') {
+                    // The user chooses a cell which isn't empty meaning --> move can't be made
+                    cerr << "\nThis cell has already been chosen, Please choose another cell\n";
+                }
+
+                // validation check to make sure the move is adjacent (horizontal or vertical)
+                if ((abs(CurrentX - x) == 1 && CurrentY == y) || (abs(CurrentY - y) == 1 && CurrentX == x)) {
+                    // if yes, the move will be stored
+                    break;
+                } else {
+                    cerr << "\nInvalid move, please choose another cell as you can only move to adjacent cells horizontally or vertically\n";
+                    continue;
+                }
+            }
+
+
             // player 2 is a random player
         } else {
-            // getting the random target position
+            // getting the random target position index
             do {
                 CurrentX = rand() % 4;
                 CurrentY = rand() % 4;
                 // validation check to make sure the index chosen randomly is the same symbol for Player 2 -> 'O'
             } while (this->board[CurrentX][CurrentY] != symbol);
 
-            // getting the random target position
+            // getting the random target position index
             do {
                 x = rand() % 4;
                 y = rand() % 4;
@@ -136,42 +240,16 @@ bool Four_times_Four_Board<T>::update_board(int x, int y, T symbol) {
         }
     }
 
-    // Validate the indexes input by the player
-    if (CurrentX < 0 || CurrentX >= this->rows || CurrentY < 0 || CurrentY >= this->columns ||
-        x < 0 || x >= this->rows || y < 0 || y >= this->columns) {
-        cout << "\n^^ Invalid move, the input is out of bounds\n";
-        return false;
-    }
-
-    // Check if the player is trying to move their own symbol
-    if (this->board[CurrentX][CurrentY] != symbol) {
-        cout << "\nInvalid move, you can only move your own token\n";
-        return false;
-    }
-
-    // Validation check to make sure that the target cell is empty
-    if (this->board[x][y] != ' ') {
-        cout << "\nInvalid move, target cell is not empty\n";
-        return false;
-    }
-
-    // validation check to make sure the move is adjacent (horizontal or vertical)
-    if ((abs(CurrentX - x) == 1 && CurrentY == y) || (abs(CurrentY - y) == 1 && CurrentX == x)) {
-        // if yes, the move will be stored
-        // Clear the current position
-        this->board[CurrentX][CurrentY] = ' ';
-        // Place the player's token at the new position
-        this->board[x][y] = toupper(symbol);
-        // Increment the number of moves to give good counter visibility for the player
-        this->n_moves++;
-        return true;
-    } else {
-        cout << "Invalid move, you can only move to adjacent cells horizontally or vertically\n";
-        return false;
-    }
+    // Clear the current position
+    this->board[CurrentX][CurrentY] = ' ';
+    // Place the player's token at the new position
+    this->board[x][y] = toupper(symbol);
+    // Increment the number of moves to give good counter visibility for the player
+    this->n_moves++;
+    return true;
 }
 
-
+/// display the board after each move
 template <typename T>
 // displaying the board
 void Four_times_Four_Board<T> :: display_board() {
@@ -201,12 +279,13 @@ void Four_times_Four_Board<T> :: display_board() {
         //putting a separator between each row
         if (i < 3) cout << "  -----------------\n";
     }
+    cout << "  -----------------\n";
     cout << endl;
 
 }
 
 
-// counting three-in-a-rows for each player
+/// counting three-in-a-rows for each player
 // will count for each player the three-in-a-rows in columns vertically and horizontally
 template <typename T>
 int Four_times_Four_Board<T>::counter_three_in_a_row(T symbol) {
@@ -255,7 +334,7 @@ int Four_times_Four_Board<T>::counter_three_in_a_row(T symbol) {
     return counter;
 }
 
-
+/// check if there is a winner
 template <typename T>
 bool Four_times_Four_Board<T>:: is_win(){
     int first = counter_three_in_a_row('X');
@@ -266,7 +345,7 @@ bool Four_times_Four_Board<T>:: is_win(){
         *global_Players[1] = *global_Players[0];
         return true;
     }
-        // second player won
+    // second player won
     else if(second>0){
         *global_Players[1] = *global_Players[1];
         return true;
@@ -275,48 +354,31 @@ bool Four_times_Four_Board<T>:: is_win(){
 }
 
 
-// there is now draw in the game, so the is draw is always false
+/// there is now draw in the game, so the is draw is always false
 template <typename T>
 bool Four_times_Four_Board<T>::is_draw() {
     return  false;
 }
 
-
-// check if the game is over meaning that a player has won
+/// check if the game is over meaning that a player has won
 template <typename T>
 bool Four_times_Four_Board<T>::game_is_over() {
     return is_win() || is_draw();
 }
 
 
-// constructor for tic Tac Toe player
+/// constructor for tic Tac Toe player
 template <typename T>
 Four_times_Four_Player<T>::Four_times_Four_Player(string name, T symbol) : Player<T>(name, symbol) {}
 
 
 
-// Get move from a human player
+/// Get move from a human player
 template <typename T>
 void Four_times_Four_Player<T>::getmove(int& x, int& y) {
     // get move is implemented in the update board
-}
+    cout << "\n-> Player '" << this->getname() << "' : Please enter the cell index of the token you want to move (x y):\n";
 
-// random player initialization
-template <class T>
-Random_fourtimesfour<T> :: Random_fourtimesfour(T symbol) : RandomPlayer<T>(symbol) {
-    this->dimension = 4;
-    this->name = "Random Computer Player";
-    srand(static_cast<unsigned int>(time(0)));  // Seed the random generator
 }
-
-// getting a random move
-template <typename T>
-void Random_fourtimesfour<T>::getmove(int& x, int& y) {
-    // Random row index
-    x = rand() % this->dimension;
-    // Random column index
-    y = rand() % this->dimension;
-}
-
 
 #endif // BOARD_GAMES_PROBLEM_7_H
