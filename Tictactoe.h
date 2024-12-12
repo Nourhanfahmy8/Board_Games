ca46661818e9ff5b0652b66849dc0bd84d885a94
Gameforlife <nourhanfahmy8@gmail.com>
2024-12-12 15:20:20 +0200

    Problem 3

--- Tictactoe.h
+++ Tictactoe.h
@@ -6,6 +6,9 @@
 
 /// ----- PROBLEM 3 : 5*5 Tic Tac Toe ----- ///
 
+Player<char>* global_Players[2];
+static bool israndom_player1 = false;
+static bool israndom_player2 = false;
 
 /// Class of 5*5 Tic Tac Toe
 template <typename T>
